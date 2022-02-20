c95cfa82c84ec57e988452fc8fcd84ba668c5a97
Sofia <s.iannicelli@byu.net>
2022-02-20 03:58:27 +0000

    implemented genre page

--- scripts/genre.js
+++ scripts/genre.js
@@ -5,27 +5,77 @@ function loadResults(e) {
 
     let selectState = document.getElementById("state-names");
     var state = selectState.options[selectState.selectedIndex].text;
+    let lstate = state.toLowerCase();
     console.log(state);
 
     let selectGenre = document.getElementById("genre-names");
     var genre = selectGenre.options[selectGenre.selectedIndex].text;
+    let lgenre = genre.toLowerCase();
     console.log(genre);
 
     let response = document.getElementById("select-response");
     response.innerHTML = "";
 
-    if(state.includes("Select") || genre.includes("Select")) {
+    if(lstate.includes("select") || lgenre.includes("select")) {
+        let content = "<p><em> Error: "
         if(state.includes("Select")) {
-            response.innerHTML += "<p><em>" + "Error: Select a state." + "</em></p>";
-            response.style.display = "inline";
+            content += "Select a state. ";
         }
         if(genre.includes("Select")) {
-            response.innerHTML += "<p><em>" + "Error: select a genre." + "</em></p>";
-            repsonse.style.display = "inline";
+            content += "Select a genre.";
         }
+        content += "</em></p>";
+        response.innerHTML = content;
+        response.style.display = "inline";
     }
     else {
+        let results = document.getElementById("results");
+        let fullurl = "https://fr1.api.radio-browser.info/json/stations/search?";
+        fullurl += "countrycode=us";
+        fullurl += "&state=" + lstate;
+        fullurl += "&tag=" + lgenre;
+        fullurl += "&limit=10";
+        console.log(fullurl);
+        fetch(fullurl)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let results = document.getElementById("results");
+            let content = "<p>Results for <strong>" + genre + "</strong> in <strong>" + state + "</strong></p>";
+            if(data.length === 0) {
+                content += "<p class='no-results'>**No results found. Try another selection.**</p>"
+            }
+            else {
+                content += "<table class=\"result-table\">"
+                content += "<tr>" + "<th>Station Name</th>" + "<th>Description</th>" + "<th>Website Link</th>" + "</tr>";
+                for(let elem of data) {
+                    content += "<tr>"
+                    content += "<td class='station-name'>" + elem.name + "</td>";
+                    content += "<td>"
+                    const initDescr = elem.tags;
+                    let descr = "";
+                    let startIndex = 0;
+                    let tagCount = 0;
+                    while(initDescr.indexOf(",", startIndex) != -1) {
+                        let commaIndex = initDescr.indexOf(",", startIndex);
+                        if(isNaN(initDescr.substring(startIndex, commaIndex)) && tagCount < 2) {
+                            descr += initDescr.substring(startIndex, commaIndex + 1) + " ";
+                            ++tagCount;
+                        }
+                        startIndex = commaIndex + 1;
+                    }
+                    descr += initDescr.substring(startIndex);
+                    content += descr + "</td>";
+                    content += "<td>";
+                    content += "<a href=\"" + elem.homepage + "\">" + elem.homepage + "</a>";
+                    content += "</td>";
+                    content += "</tr>";
+                }
+                content += "</table>"
+            }
 
+            results.innerHTML = content;
+        })
     }
 
 }
